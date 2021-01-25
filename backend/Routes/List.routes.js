58c97f5fd353e0424c2404a20c20fb7791447415
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-01-25 22:56:33 +0530

    Added Delete Route

--- backend/Routes/List.routes.js
+++ backend/Routes/List.routes.js
@@ -1,51 +1,76 @@
 const router = require("express").Router();
 const createError = require("http-errors");
+const User = require("../models/user.model");
 const List = require("../models/list.model");
-const { userSchema } = require("../helper/validation__schema");
+const { userSchema, listSchema } = require("../helper/validation__schema");
+const { genAccessToken, verifyAccessToken } = require("../helper/jwt_helper");
 
 router.post("/register", async (req, res, next) => {
   try {
-    console.log(req.body);
     const result = await userSchema.validateAsync(req.body);
-    const doesEmailExist = await List.findOne({
+    const doesEmailExist = await User.findOne({
       email: result.email,
     });
     if (doesEmailExist)
       throw createError.Conflict("Email Address already exist");
-    const doesMobileExist = await List.findOne({
+    const doesMobileExist = await User.findOne({
       mobile: result.mobile,
     });
     console.log("yes = ", doesMobileExist);
-    if (doesMobileExist) throw createError.Conflict("Mobile mber exist");
-    const user = await new List(result);
-    await user.save();
-    console.log("user = ", user);
-    res.status(200).send(user);
+    if (doesMobileExist) throw createError.Conflict("Mobile number exist");
+    const user = await new User(result);
+    const savedUser = await user.save();
+    const token = await genAccessToken(savedUser._id);
+    res.status(200).send(token);
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/login", async (req, res, next) => {});
-
-router.post("/addList", async (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   try {
-    console.log("Body = ", req.body);
-    const result = await userSchema.validateAsync(req.body);
-    console.log("result = ", result);
-    const list = await new List(result);
-    console.log("new list = ", list);
-    await list.save();
-    res.status(200).send("List Saved Successfullly");
+    if (!req.body.email || !req.body.password)
+      throw createError.BadRequest("Email, Password both fields are required");
+    const user = await User.findOne({ email: req.body.email });
+    console.log(user);
+    if (!user) throw createError.NotFound("User not Found");
+    const isPasswordValid = await user.verifyPassword(req.body.password);
+    if (!isPasswordValid) throw createError.NotFound("User Not Found");
+    const token = await genAccessToken(user._id);
+    res.status(200).send(token);
   } catch (error) {
-    console.log("err = ", error);
     next(error);
   }
 });
 
-router.get("/getAllList", async (req, res, next) => {
+router.post("/addList", verifyAccessToken, async (req, res, next) => {
   try {
-    const lists = await List.find();
+    const result = await listSchema.validateAsync({
+      email: req.payload.email,
+      list: req.body.list,
+    });
+    const isList = await List.findOne({ email: result.email });
+    if (!isList) {
+      const newList = await new List(result);
+      await newList.save();
+      res.status(200).send("List Added");
+    } else {
+      await isList.list.push(result.list);
+      await isList.save();
+      res.status(200).send("List Added");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/getAllList", verifyAccessToken, async (req, res, next) => {
+  try {
+    console.log(req.payload);
+    const lists = await List.findOne({
+      email: req.payload.email,
+    });
+    if (!lists) res.status(200).send("nothing is added");
     console.log(lists);
     res.status(200).send(lists);
   } catch (error) {
@@ -53,13 +78,12 @@ router.get("/getAllList", async (req, res, next) => {
   }
 });
 
-router.delete("/deleteList", async (req, res, next) => {
+router.delete("/deleteList", verifyAccessToken, async (req, res, next) => {
   try {
-    const list = await List.findOneAndDelete({
-      _id: req.body.id,
-    });
-    console.log(list);
+    console.log("User = ", req.payload);
+    const list = List.findOne({ email: req.payload.email });
     if (!list) throw createError.NotFound();
+
     res.send("Successfully Deleted");
   } catch (error) {
     next(error);
