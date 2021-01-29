5ef7183d7e25cf9f0c50a945b51a5981ac6669f8
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-01-29 21:02:30 +0530

    added models

--- backend/models/list.model.js
+++ backend/models/list.model.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var listSchema = new mongoose.Schema({
-  list: {
-    type: String,
-    trim: true,
+var listSchema = new mongoose.Schema(
+  {
+    list: {
+      type: String,
+      trim: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 //Export the model
 listSchema.plugin(require("mongoose-autopopulate"));
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,33 +2,36 @@ const mongoose = require("mongoose"); // Erase if already required
 const bcrypt = require("bcrypt");
 const List = require("./list.model");
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-  fName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  lists_to_add: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "List",
+var userSchema = new mongoose.Schema(
+  {
+    fName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-});
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lists_to_add: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "List",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   try {
