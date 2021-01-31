8a8abbe29240f4add8ee96bd9e36b7b1078dbba3
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-01-31 22:16:10 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status = err.status || 500;
+  res.status(err.status || 500);
   console.log("err = ", err);
   res.send({
     status: err.status || 500,
