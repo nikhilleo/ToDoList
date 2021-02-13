c56410d2cf2ab332010c29bdd85d0204de04fde9
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-13 11:49:40 +0530

    done

--- backend/helper/genJWT_keys.js
+++ backend/helper/genJWT_keys.js
@@ -2,6 +2,5 @@ const crypto = require("crypto");
 
 const key1 = crypto.randomBytes(32).toString("hex");
 const key2 = crypto.randomBytes(32).toString("hex");
-  console.log("Appd started");
 
 console.table({ key1, key2 });
